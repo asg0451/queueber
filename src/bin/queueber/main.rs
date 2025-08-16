8e8d3fdd8a9668322af72feaf566e9dd4efd659d
Miles Frankel <miles.frankel@cockroachlabs.com>
2025-08-17 01:51:12 +0200

    rw

--- src/bin/queueber/main.rs
+++ src/bin/queueber/main.rs
@@ -6,6 +6,7 @@ fn main() -> Result<()> {
     use crate::queueber_capnp::item;
     use capnp::serialize_packed;
 
+    // create & write
     let mut message = ::capnp::message::Builder::new_default();
     let mut item = message.init_root::<item::Builder>();
     item.set_contents(b"hello");
@@ -13,6 +14,17 @@ fn main() -> Result<()> {
     item.set_id(42);
 
     serialize_packed::write_message(&mut ::std::io::stdout(), &message)?;
+
+    let mut buf = Vec::new();
+    serialize_packed::write_message(&mut buf, &message)?;
+
+    // read
+    let message_reader =
+        serialize_packed::read_message(&buf[..], ::capnp::message::ReaderOptions::new())?;
+
+    let item = message_reader.get_root::<item::Reader>()?;
+
+    println!("{:?}", item.get_contents()?);
 
     Ok(())
 }
